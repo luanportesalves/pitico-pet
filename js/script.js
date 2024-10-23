28c591ae1e54309731dc93e13db3588d9ff77307
luanportesalves <lportes550@gmail.com>
2024-10-23 20:21:32 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,18 @@
-let carosseis = document.getElementsByClassName ('carossel-Container')
 
-for(let i = 0; i < carosseis.length; i++){
-  let carossel = carosseis[i]
-  let btnBack = carossel.getElementsByClassName('BtnBack')[0]
-  let btnNext = carossel.getElementsByClassName('BtnNext')[0]
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+document.getElementById('radio1').checked = true
+setInterval(() => {
+  proximaImg()
 
-  let itens = carossel.getElementsByClassName('item')
-  let posicaoAnterior = 0
-  let mover = posicaoAnterior + 100
+}, 5000)
 
-  btnNext.addEventListener('click', ()=>{
-    mover = posicaoAnterior + 100
+function proximaImg() {
+    cont++
+    if(cont > 3){
+    
+      cont = 1
+    }
+    document.getElementById('radio'+ cont).checked = true
 
-    for(let c = 0; c < itens.length; c++ ){
-
-    itens[c].style.right=  mover + '%'
-
-    posicaoAnterior = mover}
-  })
-
-  btnBack.addEventListener('click', ()=>{
-    mover = posicaoAnterior - 100
-
-    for(let c = 0; c < itens.length; c++ ){
-
-    itens[c].style.right=  mover + '%'
-
-    posicaoAnterior = mover}
-
-  })
 }
